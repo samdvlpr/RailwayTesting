105f901febd2e08b48777763cfd7bb01aa692d4d
Sam Blundell <6898117+samdvlpr@users.noreply.github.com>
2023-10-10 20:26:05 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -23,11 +23,11 @@ sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' 
 ssh-keygen -A
 
 # Add Vault
-sudo apt install gpg
+sudo apt-get install gpg
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-sudo apt install vault
+sudo apt-get update
+sudo apt-get install vault
 
 exec /usr/sbin/sshd -D -e "$@"
