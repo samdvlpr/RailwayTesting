ac75511904964f3dfd27c2a7f2378afee4101bec
Sam Blundell <6898117+samdvlpr@users.noreply.github.com>
2023-10-10 19:44:57 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,21 @@
 echo -n "$Username:$Password"
 
-apk add --update --no-cache openssh
+apt install -y openssh-server
 
-echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
+mkdir /var/run/sshd
+
+# echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
 
 adduser -h /home/$Username -s /bin/sh -D $Username
 
 echo -n "$Username:$Password" | chpasswd
 
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+
 ssh-keygen -A
 
 exec /usr/sbin/sshd -D -e "$@"
+
+sudo apt update && sudo apt install gpg
