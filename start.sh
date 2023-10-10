8da6d6d1053bb23251264b197506be75d799a055
Sam Blundell <6898117+samdvlpr@users.noreply.github.com>
2023-10-10 20:10:16 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ sudo useradd -s /bin/bash -d /home/$Username/ -m -G sudo $Username
 
 echo -n "$Username:$Password" | sudo chpasswd
 
-sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+# sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 
 sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
